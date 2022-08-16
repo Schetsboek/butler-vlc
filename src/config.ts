5ba0e6b43594ad1882c8a2ca4ea72bbd00db9b42
Jeroen Schets <jeroen.schets+git@gmail.com>
2022-08-16 22:53:55 +0200

    Removed unused config, updated version

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,6 @@ require('dotenv').config({ path: './.env' });
 
 export default {
   PORT: process.env.PORT || '3030',
-  VERSION: process.env.VERSION || '1.0',
-  VLC_PORT: process.env.VLC_PORT || '8080',
-  VLC_HOST: process.env.VLC_HOST || '127.0.0.1',
-  VLC_USERNAME: process.env.VLC_USERNAME || '',
-  VLC_PASSWORD: process.env.VLC_PASSWORD || '',
+  VERSION: process.env.VERSION || '1.1',
   MUSIC_FOLDER: process.env.MUSIC_FOLDER || '/home/schetsboek/music',
 }
